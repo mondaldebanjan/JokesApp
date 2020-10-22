bdeb4797021aef6e1579f2dbcae9b333d3f274ad
Debanjan Mondal <mondal.debanjan.bwn@gmail.com>
2020-10-22 09:48:29 +0530

    Added Service controller and HTML

--- src/main/java/com/debanjan/jokesapp/configuration/ChuckConfig.java
+++ src/main/java/com/debanjan/jokesapp/configuration/ChuckConfig.java
@@ -3,7 +3,7 @@ package com.debanjan.jokesapp.configuration;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//needed r adding external bean
 @Configuration
 public class ChuckConfig {
 
